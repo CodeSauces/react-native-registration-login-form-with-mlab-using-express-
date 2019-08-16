465610401985058302c02a6027d3dae823a38fc0
Hassan Amin <hasan_amin3@yahoo.com>
2019-08-16 18:36:09 +0500

    registration done

--- App.js
+++ App.js
@@ -1,114 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { TextInput, View, Picker, Button, Text, Alert } from 'react-native';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: undefined,
+      password: undefined,
+      email: undefined,
+      gender: undefined,
+    };
+    _onPressSubmit = () => {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+      var ObjToSend = {
+        name: this.state.name,
+        password: this.state.password,
+        email: this.state.email,
+        gender: this.state.gender,
+      }
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+      var options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(ObjToSend),
+      };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+      fetch('http://192.168.0.117:8000/formData', options).then((res) => {
+        Alert.alert("Response Came");
+      }).catch(function (error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+        throw error;
+      });
+    }
+  }
 
-export default App;
+  render() {
+    return (
+
+      <View>
+        <TextInput
+          placeholder="Name"
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(name) => this.setState({ name })}
+          value={this.state.name}
+        />
+        <TextInput
+          placeholder="Password"
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(password) => this.setState({ password })}
+          value={this.state.password}
+        />
+        <TextInput
+          placeholder="Email"
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(email) => this.setState({ email })}
+          value={this.state.email}
+        />
+
+        <Picker
+          selectedValue={this.state.gender}
+          style={{ height: 50, width: 120 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ gender: itemValue })
+          }>
+          <Picker.Item label="Male" value="male" />
+          <Picker.Item label="Female" value="female" />
+        </Picker>
+        <Button
+          onPress={_onPressSubmit}
+          title="Submit"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
+}
